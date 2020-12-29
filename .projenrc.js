636bfdd2a81ec8a96c703461757d37e247889aca
Pahud <pahudnet@gmail.com>
2020-12-29 13:33:26 +0800

    update

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,10 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
+const AUTOMATION_TOKEN = 'GITHUB_TOKEN';
+
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: "1.75.0",
-  name: "aws-cdk-eks-sample",
+  cdkVersion: '1.75.0',
+  name: 'aws-cdk-eks-sample',
   cdkDependencies: [
     '@aws-cdk/aws-autoscaling',
     '@aws-cdk/aws-ec2',
@@ -10,6 +12,46 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-iam',
   ],
   dependabot: false,
+});
+
+// create a custom projen and yarn upgrade workflow
+workflow = project.github.addWorkflow('ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '11 0 * * *',
+  }], // 0:11am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      { uses: 'actions/checkout@v2' },
+      {
+        uses: 'actions/setup-node@v1',
+        with: {
+          'node-version': '10.17.0',
+        },
+      },
+      { run: 'yarn upgrade' },
+      { run: 'yarn projen:upgrade' },
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+          'labels': 'auto-merge',
+        },
+      },
+    ],
+  },
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
